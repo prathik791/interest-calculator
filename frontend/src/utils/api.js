54c9c3283278d56d87db627867597ede46145ee7
prathik <prathikkumar2004@gmail.com>
2026-03-05 20:23:31 +0300

    ppp

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+  baseURL: "https://interest-calculator-backend-i2dr.onrender.com/api"
 });
-
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
