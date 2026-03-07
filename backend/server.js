d0b0c4682e5b43e04b4e5bc6c83a26951ba3a5ac
prathik <prathikkumar2004@gmail.com>
2026-03-07 08:28:18 +0300

    midd

--- backend/server.js
+++ backend/server.js
@@ -24,11 +24,7 @@ if (!fs.existsSync(uploadsDir)) fs.mkdirSync(uploadsDir, { recursive: true });
 
 
 
-app.use(cors({
-  origin: "*",
-  methods: ["GET","POST","PUT","DELETE"],
-  allowedHeaders: ["Content-Type","Authorization"]
-}));
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
